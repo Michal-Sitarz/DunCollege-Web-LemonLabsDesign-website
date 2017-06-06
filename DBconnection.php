47bfb9011bc08d1a6046918246e50152dd9904f9
Michal Sitarz <me@michalsitarz.com>
2017-06-06 21:24:24 +0100

    password invisible
    Password characters must be hidden in public file!

--- DBconnection.php
+++ DBconnection.php
@@ -3,7 +3,7 @@
 		$host = 'studentnet.dundeeandangus.ac.uk';
 		$dbname = 'db_1507342';
 		$username = '1507342';
-		$password = 'db4w3s0m3';
+		$password = '•••••••';
 		
 		$con = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8',$username,$password);
 	}
